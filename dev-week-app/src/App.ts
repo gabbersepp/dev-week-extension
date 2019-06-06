7cf1d9aeac440740f6fafdc79626e190cd59b388
Josef Biehler <jbiehler@samhammer.de>
2019-06-06 16:47:51 +0200

    add utils

--- dev-week-app/src/App.ts
+++ dev-week-app/src/App.ts
@@ -1,4 +1,5 @@
 import { Component, Vue } from 'vue-property-decorator';
+import { sleep } from './Utils';
 
 @Component({
   components: {
@@ -7,4 +8,11 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 export default class App extends Vue {
 
+  private data: any[] = [];
+
+  // trigger reading of calendar
+  private async init(): Promise<void> {
+    await sleep(5000);
+    //read data
+  }
 }
